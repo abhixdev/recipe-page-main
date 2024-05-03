f1bae6c851ccfdf1b7957b767e1d9a7b64294b8e
Abhishek Kumar <abhishek117293@gmail.com>
2024-05-03 19:26:22 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      md: "375px",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+      serif: ["Young Serif", "serif"],
+    },
+    extend: {
+      colors: {
+        Nutmeg: "hsl(14, 45%, 36%)",
+        DarkRaspberry: "hsl(332, 51%, 32%)",
+        RoseWhite: "hsl(330, 100%, 98%)",
+        Eggshell: "hsl(30, 54%, 90%)",
+        LightGrey: "hsl(30, 18%, 87%)",
+        WengeBrown: "hsl(30, 10%, 34%)",
+        DarkCharcoal: "hsl(24, 5%, 18%)",
+      },
+    },
+  },
+  plugins: [],
+};
